715964f912c84be38ae017384ff575d8896b35b9
UnarovAital <Unaait@mail.ru>
2020-09-22 20:17:50 +0300

    first test

--- Lab3/smile.py
+++ Lab3/smile.py
@@ -1,5 +1,25 @@
-import pygame as pg
+import pygame
+from pygame.draw import *
 
-pg.init()
+pygame.init()
 
-screen = pg.display.set_mode((400,400))
+FPS = 30
+screen = pygame.display.set_mode((400, 400))
+
+circle(screen, (255, 255, 0), (200, 200), 50, 50)
+circle(screen, (255, 0, 0), (180, 180), 10, 5,)
+circle(screen, (255, 0, 0), (220, 180), 10, 5,)
+circle(screen, (255, 0, 0), (180, 180), 10, 5,)
+circle(screen, (255, 0, 0), (220, 180), 10, 5,)
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
