85d3747495c47d633158e69a5ca11e518fd481ae
kodirovat <bil.zlata@gmail.com>
2020-09-29 15:41:11 +0300

    color palette

--- Lab4/Picture2.py
+++ Lab4/Picture2.py
@@ -11,45 +11,57 @@ hedgehog_width = 220
 hedgehog_height = 80
 screen = pg.display.set_mode((1000, 700))
 
+# Цвета
+white = (255, 255, 255)
+black = (0, 0, 0)
+green = (84, 200, 117)
+gray = (70, 70, 70)
+yellow = (236, 205, 0)
+brown = (68, 0, 19)
+orange = (230, 145, 0)
+dark_gray = (30, 30, 30)
+light_gray = (100, 100, 100)
+light_orange = (255, 127, 116)
+
 # Создаем фон
-pg.draw.rect(screen, (84, 200, 117), (0, 0, screen_width, screen_height))
-pg.draw.rect(screen, (70, 70, 70), (0, 400, screen_width, screen_height))
-pg.draw.rect(screen, (236, 205, 0), (int(screen_width * 0.75), 0, 100, 440))
-pg.draw.rect(screen, (236, 205, 0), (int(screen_width * 0.1), 0, 200, 650))
-pg.draw.rect(screen, (236, 205, 0), (int(screen_width * 0.9), 0, 60, 520))
-pg.draw.rect(screen, (236, 205, 0), (int(screen_width * 0), 0, 40, 440))
+pg.draw.rect(screen, green, (0, 0, screen_width, screen_height))
+pg.draw.rect(screen, gray, (0, 400, screen_width, screen_height))
+pg.draw.rect(screen, yellow, (int(screen_width * 0.75), 0, 100, 440))
+pg.draw.rect(screen, yellow, (int(screen_width * 0.1), 0, 200, 650))
+pg.draw.rect(screen, yellow, (int(screen_width * 0.9), 0, 60, 520))
+pg.draw.rect(screen, yellow, (int(screen_width * 0), 0, 40, 440))
 
 
 # Создаем функцию, создающую ежа
 def hedgehog(x, y):
     a = hedgehog_width / 2.3
     b = hedgehog_height / 2.3
-    pg.draw.ellipse(screen, (68, 0, 19), (screen_width * x, screen_height * y, hedgehog_width, hedgehog_height))
-    pg.draw.ellipse(screen, (100, 100, 100), (screen_width * x, screen_height * y, hedgehog_width, hedgehog_height), 3)
+    pg.draw.ellipse(screen, brown, (screen_width * x, screen_height * y, hedgehog_width, hedgehog_height))
+    pg.draw.ellipse(screen, light_gray, (screen_width * x, screen_height * y, hedgehog_width, hedgehog_height), 3)
 
-    pg.draw.ellipse(screen, (68, 0, 19),
+    pg.draw.ellipse(screen, brown,
                     (screen_width * x + hedgehog_width * 0.9, screen_height * y + hedgehog_height * 0.4, 50, 30))
-    pg.draw.ellipse(screen, (0, 0, 0),
+    pg.draw.ellipse(screen, black,
                     (screen_width * x + hedgehog_width * 0.9, screen_height * y + hedgehog_height * 0.4, 50, 30), 1)
 
-    pg.draw.ellipse(screen, (0, 0, 0),
+    pg.draw.ellipse(screen, black,
                     (screen_width * x + hedgehog_width + 13, screen_height * y + hedgehog_height * 0.4 + 6, 8, 8))
-    pg.draw.ellipse(screen, (0, 0, 0),
+    pg.draw.ellipse(screen, black,
                     (screen_width * x + hedgehog_width + 2, screen_height * y + hedgehog_height * 0.4 + 10, 8, 8))
-    pg.draw.ellipse(screen, (0, 0, 0),
+    pg.draw.ellipse(screen, black,
                     (screen_width * x + hedgehog_width + 23, screen_height * y + hedgehog_height * 0.4 + 11, 6, 6))
-    pg.draw.ellipse(screen, (255, 255, 255),
+    pg.draw.ellipse(screen, white,
                     (screen_width * x + hedgehog_width + 13, screen_height * y + hedgehog_height * 0.4 + 6, 8, 8), 1)
-    pg.draw.ellipse(screen, (255, 255, 255),
+    pg.draw.ellipse(screen, white,
                     (screen_width * x + hedgehog_width + 2, screen_height * y + hedgehog_height * 0.4 + 10, 8, 8), 1)
-    pg.draw.ellipse(screen, (255, 255, 255),
+    pg.draw.ellipse(screen, white,
                     (screen_width * x + hedgehog_width + 23, screen_height * y + hedgehog_height * 0.4 + 11, 6, 6), 1)
 
-    pg.draw.ellipse(screen, (68, 0, 19), (screen_width * x, screen_height * y + hedgehog_height - 20, 40, 20))
-    pg.draw.ellipse(screen, (100, 100, 100), (screen_width * x, screen_height * y + hedgehog_height - 20, 40, 20), 2)
-    pg.draw.ellipse(screen, (68, 0, 19),
+    pg.draw.ellipse(screen, brown, (screen_width * x, screen_height * y + hedgehog_height - 20, 40, 20))
+    pg.draw.ellipse(screen, light_gray, (screen_width * x, screen_height * y + hedgehog_height - 20, 40, 20), 2)
+    pg.draw.ellipse(screen, brown,
                     (screen_width * x + hedgehog_width - 35, screen_height * y + hedgehog_height - 20, 40, 20))
-    pg.draw.ellipse(screen, (100, 100, 100),
+    pg.draw.ellipse(screen, light_gray,
                     (screen_width * x + hedgehog_width - 35, screen_height * y + hedgehog_height - 20, 40, 20), 2)
 
     for i in range(30):
@@ -59,12 +71,12 @@ def hedgehog(x, y):
         A = [screen_width * x + a + x0, screen_height * y + b + y0]
         B = [screen_width * x + a + x0 + 20 * np.cos(k), screen_height * y + b + y0 - 20 * np.sin(k)]
         C = [screen_width * x + a + x0 + 5 - 90 * np.sin(k), screen_height * y + b + y0 - 90 * np.cos(k)]
-        pg.draw.polygon(screen, (30, 30, 30), [A, B, C], )
-        pg.draw.polygon(screen, (0, 0, 0), [A, B, C], 1)
+        pg.draw.polygon(screen, dark_gray, [A, B, C], )
+        pg.draw.polygon(screen, black, [A, B, C], 1)
 
     pg.draw.ellipse(screen, (255, 33, 0),
                     (screen_width * x + hedgehog_width - 100, screen_height * y + hedgehog_height - 140, 70, 70))
-    pg.draw.ellipse(screen, (230, 145, 0),
+    pg.draw.ellipse(screen, orange,
                     (screen_width * x + hedgehog_width - 200, screen_height * y + hedgehog_height - 140, 50, 50))
 
     for i in range(20):
@@ -74,12 +86,12 @@ def hedgehog(x, y):
         A = [screen_width * x + a + x0, screen_height * y + b + y0]
         B = [screen_width * x + a + x0 + 20 * np.cos(k), screen_height * y + b + y0 - 20 * np.sin(k)]
         C = [screen_width * x + a + x0 + 5 - 90 * np.sin(k), screen_height * y + b + y0 - 90 * np.cos(k)]
-        pg.draw.polygon(screen, (30, 30, 30), [A, B, C], )
-        pg.draw.polygon(screen, (0, 0, 0), [A, B, C], 1)
+        pg.draw.polygon(screen, dark_gray, [A, B, C], )
+        pg.draw.polygon(screen, black, [A, B, C], 1)
 
-    pg.draw.ellipse(screen, (255, 255, 255),
+    pg.draw.ellipse(screen, white,
                     (screen_width * x + hedgehog_width - 140, screen_height * y + hedgehog_height - 160, 20, 70))
-    pg.draw.ellipse(screen, (255, 127, 116),
+    pg.draw.ellipse(screen, light_orange,
                     (screen_width * x + hedgehog_width - 175, screen_height * y + hedgehog_height - 160, 90, 30))
 
     for i in range(10):
@@ -89,7 +101,7 @@ def hedgehog(x, y):
         A = [screen_width * x + a + x0, screen_height * y + b + y0]
         B = [screen_width * x + a + x0 + 20 * np.cos(k), screen_height * y + b + y0 - 20 * np.sin(k)]
         C = [screen_width * x + a + x0 + 5 - 90 * np.sin(k), screen_height * y + b + y0 - 90 * np.cos(k)]
-        pg.draw.polygon(screen, (30, 30, 30), [A, B, C], )
+        pg.draw.polygon(screen, dark_gray, [A, B, C], )
         pg.draw.polygon(screen, (0, 0, 0), [A, B, C], 1)
 
 
